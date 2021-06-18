b08848c24234f351db1e126978a7171ce89278d4
AmanDesai10 <amandesai1002@gmail.com>
2021-06-19 02:00:00 +0530

    Added basic homepage

---
+++ lib/models/add_device_model.dart
@@ -0,0 +1,12 @@
+import 'package:meta/meta.dart';
+import 'package:flutter/material.dart';
+
+class AddDevice {
+  final Icon? icon;
+  final String? text;
+
+  const AddDevice({
+    @required this.icon,
+    @required this.text,
+  });
+}
--- lib/screens/add_device.dart
+++ lib/screens/add_device.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:smart_home_app/devicelist.dart';
+import 'package:smart_home_app/models/add_device_model.dart';
 
 class AddDeviceScreen extends StatefulWidget {
   const AddDeviceScreen({Key? key}) : super(key: key);
@@ -12,6 +14,32 @@ class _AddDeviceScreenState extends State<AddDeviceScreen> {
   Widget build(BuildContext context) {
     return Container(
       color: Colors.blue,
+      child: Column(children: [
+        ElevatedButton(
+          child: Text("Insert"),
+          onPressed: () {
+            insertdevice();
+          },
+        ),
+        ElevatedButton(
+          child: Text("Remove"),
+          onPressed: () {
+            removedevice();
+          },
+        ),
+      ]),
     );
   }
+}
+
+AddDevice a =
+    AddDevice(icon: Icon(Icons.desktop_windows_outlined), text: "For AC");
+
+void insertdevice() {
+  DeviceData.device_list.insert(DeviceData.device_list.length, a);
+  // key.currentState.insertdevice(DeviceData.device_list.length);
+}
+
+void removedevice() {
+  DeviceData.device_list.removeAt(DeviceData.device_list.length - 1);
 }
